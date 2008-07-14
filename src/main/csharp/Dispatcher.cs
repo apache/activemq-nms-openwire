1a556eab75e441e7d9b63f689560c735c04e5caa
Jim Gomes <jgomes@apache.org>
2008-07-14 19:50:43 +0000

    Upgrade to VS2008 build scripts.

--- src/main/csharp/Dispatcher.cs
+++ src/main/csharp/Dispatcher.cs
@@ -133,7 +133,7 @@ namespace Apache.NMS.ActiveMQ
 
             while (!bClosed && rc == null)
             {
-                if( !messageReceivedEventHandle.WaitOne(timeout, false))
+                if( !messageReceivedEventHandle.WaitOne((int) timeout.TotalMilliseconds, false))
                 {
                     break;
                 }
