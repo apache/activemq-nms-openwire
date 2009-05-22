130e56271614ae0f623dc1969f0a15db5d658e32
Jim Gomes <jgomes@apache.org>
2009-05-22 17:13:00 +0000

    Fix sample code files to not compile into main product.  Added additional comments for clarification of intent.

--- src/test/csharp/TestMain.cs
+++ src/test/csharp/TestMain.cs
@@ -18,6 +18,7 @@
 // NOTE: Keep leading spaces.  Do not convert to tabs.  This file
 // is auto-linked on the NMS website as example code.
 
+#if false	// This file should not be compiled into the product.  Demo only.
 // START SNIPPET: demo
 using System;
 using Apache.NMS;
@@ -25,67 +26,71 @@ using Apache.NMS.Util;
 
 namespace Apache.NMS.ActiveMQ.Test
 {
-    public class TestMain
+public class TestMain
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        // Example connection strings:
+        //    activemq:tcp://activemqhost:61616
+        //    activemq:tcp://activemqhost:61613?transport.wireformat=stomp
+        //    ems:tcp://tibcohost:7222
+        //    msmq://localhost
+
+        Uri connecturi = new Uri("activemq:tcp://activemqhost:61616");
+        
+        Console.WriteLine("About to connect to " + connecturi);
+
+        // NOTE: ensure the nmsprovider-activemq.config file exists in the executable folder.
+        IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+
+        using(IConnection connection = factory.CreateConnection())
+        using(ISession session = connection.CreateSession())
         {
-            try
+             // Examples for getting a destination:
+             //
+             // Hard coded destinations:
+             //    IDestination destination = session.GetQueue("FOO.BAR");
+             //    IDestination destination = session.GetTopic("FOO.BAR");
+             //
+             // Embedded destination type in the name:
+             //    IDestination destination = SessionUtil.GetDestination(session, "queue://FOO.BAR");
+             //    IDestination destination = SessionUtil.GetDestination(session, "topic://FOO.BAR");
+             //
+             // Defaults to queue if type is not specified:
+             //    IDestination destination = SessionUtil.GetDestination(session, "FOO.BAR");
+
+            IDestination destination = SessionUtil.GetDestination(session, "queue://FOO.BAR");
+            Console.WriteLine("Using destination: " + destination);
+
+            // Create a consumer and producer
+            using(IMessageConsumer consumer = session.CreateConsumer(destination))
+            using(IMessageProducer producer = session.CreateProducer(destination))
             {
-                Uri connecturi = new Uri("activemq:tcp://activemqhost:61616");
-                
-                Console.WriteLine("About to connect to " + connecturi);
+                producer.Persistent = true;
 
-                // NOTE: ensure the nmsprovider-activemq.config file exists in the executable folder.
-                IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+                // Send a message
+                ITextMessage request = session.CreateTextMessage("Hello World!");
+                request.NMSCorrelationID = "abc";
+                request.Properties["NMSXGroupID"] = "cheese";
+                request.Properties["myHeader"] = "Cheddar";
 
-                using(IConnection connection = factory.CreateConnection())
-                using(ISession session = connection.CreateSession())
+                producer.Send(request);
+
+                // Consume a message
+                ITextMessage message = consumer.Receive() as ITextMessage;
+                if(message == null)
                 {
-                    /*
-                     * Examples for getting a destination:
-                     *   IDestination destination = session.GetQueue("FOO.BAR");  // Hard coded to queue destination
-                     *   IDestination destination = session.GetTopic("FOO.BAR");  // Hard coded to topic destination
-                     *   IDestination destination = SessionUtil.GetDestination(session, "queue://FOO.BAR");  // Allows destination type to be embedded in name
-                     *   IDestination destination = SessionUtil.GetDestination(session, "topic://FOO.BAR");  // Allows destination type to be embedded in name
-                     *   IDestination destination = SessionUtil.GetDestination(session, "FOO.BAR");          // Defaults to queue if type not specified.
-                     */
-                    IDestination destination = SessionUtil.GetDestination(session, "queue://FOO.BAR");
-                    Console.WriteLine("Using destination: " + destination);
-
-                    // Create a consumer and producer
-                    using(IMessageConsumer consumer = session.CreateConsumer(destination))
-                    using(IMessageProducer producer = session.CreateProducer(destination))
-                    {
-                        producer.Persistent = true;
-
-                        // Send a message
-                        ITextMessage request = session.CreateTextMessage("Hello World!");
-                        request.NMSCorrelationID = "abc";
-                        request.Properties["NMSXGroupID"] = "cheese";
-                        request.Properties["myHeader"] = "Cheddar";
-
-                        producer.Send(request);
-
-                        // Consume a message
-                        ITextMessage message = consumer.Receive() as ITextMessage;
-                        if(message == null)
-                        {
-                            Console.WriteLine("No message received!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Received message with ID:   " + message.NMSMessageId);
-                            Console.WriteLine("Received message with text: " + message.Text);
-                        }
-                    }
+                    Console.WriteLine("No message received!");
                 }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine("Caught: " + e);
-                Console.WriteLine("Stack: " + e.StackTrace);
+                else
+                {
+                    Console.WriteLine("Received message with ID:   " + message.NMSMessageId);
+                    Console.WriteLine("Received message with text: " + message.Text);
+                }
             }
         }
     }
 }
+}
 // END SNIPPET: demo
+#endif
