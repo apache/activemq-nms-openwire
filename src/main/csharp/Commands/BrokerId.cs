cfe610051d89c23cabb06b857499475e96972254
Timothy A. Bish <tabish@apache.org>
2009-12-17 15:41:30 +0000

    Update BrokerId to customize the ToString method.

--- src/main/csharp/Commands/BrokerId.cs
+++ src/main/csharp/Commands/BrokerId.cs
@@ -54,8 +54,7 @@ namespace Apache.NMS.ActiveMQ.Commands
         ///
         public override string ToString()
         {
-            return GetType().Name + "[ " + 
-                "Value = " + Value + " ]";
+            return this.value;
         }
 
         public string Value
