8d1222759d8e4cf3fd7285483e3457403bcc406a
Timothy A. Bish <tabish@apache.org>
2009-12-21 20:52:22 +0000

    https://issues.apache.org/activemq/browse/AMQNET-212
    Disable the Inactivity Monitor by default due to stability issues.

--- src/main/csharp/Transport/Tcp/TcpTransportFactory.cs
+++ src/main/csharp/Transport/Tcp/TcpTransportFactory.cs
@@ -43,7 +43,7 @@ namespace Apache.NMS.ActiveMQ.Transport.Tcp
         /// <summary>
         /// Should the Inactivity Monitor be enabled on this Transport.
         /// </summary>
-        private bool useInactivityMonitor = true;
+        private bool useInactivityMonitor = false;
         public bool UseInactivityMonitor
         {
            get { return this.useInactivityMonitor; }
