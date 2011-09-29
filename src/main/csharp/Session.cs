64a8251b1f84f71475d47a1e089fcb028bfc88bc
Jim Gomes <jgomes@apache.org>
2011-09-29 18:47:04 +0000

    Add lock of SyncRoot when iterating over the consumers list. Fixes [AMQNET-342]. (See https://issues.apache.org/jira/browse/AMQNET-342)

--- src/main/csharp/Session.cs
+++ src/main/csharp/Session.cs
@@ -824,10 +824,13 @@ namespace Apache.NMS.ActiveMQ
 
         public void Start()
         {
-            foreach(MessageConsumer consumer in this.consumers.Values)
-            {
-                consumer.Start();
-            }
+			lock(this.consumers.SyncRoot)
+			{
+				foreach(MessageConsumer consumer in this.consumers.Values)
+				{
+					consumer.Start();
+				}
+			}
 
             if(this.executor != null)
             {
@@ -977,13 +980,16 @@ namespace Apache.NMS.ActiveMQ
 
         internal bool IsInUse(ActiveMQTempDestination dest)
         {
-            foreach(MessageConsumer consumer in this.consumers.Values)
-            {
-                if(consumer.IsInUse(dest))
-                {
-                    return true;
-                }
-            }
+			lock(this.consumers.SyncRoot)
+			{
+				foreach(MessageConsumer consumer in this.consumers.Values)
+				{
+					if(consumer.IsInUse(dest))
+					{
+						return true;
+					}
+				}
+			}
 
             return false;
         }
