0d7a02b70746289f03d1f050d1c1804f735e56a0
Timothy A. Bish <tabish@apache.org>
2013-05-17 17:21:05 +0000

    fix for: https://issues.apache.org/jira/browse/AMQNET-419
    Begins to partition the DTC ugliness into a NetTXMessageConsumer so we don't have to impact performance of the non-DTC consumer in order to deal with all DTC's issues.

---
+++ src/main/csharp/NetTxMessageConsumer.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Apache.NMS.ActiveMQ.Commands;
+
+namespace Apache.NMS.ActiveMQ
+{
+    class NetTxMessageConsumer : MessageConsumer
+    {
+        private readonly NetTxSession session;
+
+        internal NetTxMessageConsumer(Session session, ConsumerId id, ActiveMQDestination destination, 
+                                      string name, string selector, int prefetch, int maxPendingMessageCount,
+                                      bool noLocal, bool browser, bool dispatchAsync) : 
+            base(session, id, destination, name, selector, prefetch,
+                 maxPendingMessageCount, noLocal, browser, dispatchAsync)
+        {
+            this.session = session as NetTxSession;
+        }
+
+        public override void Close()
+        {
+            if (this.Closed)
+            {
+                return;
+            }
+
+            lock (this.session.TransactionContext.SyncRoot)
+            {
+                if (this.session.IsTransacted || this.session.TransactionContext.InTransaction)
+                {
+                    Tracer.DebugFormat("Consumer {0} Registering new ConsumerCloseSynchronization",
+                                       this.ConsumerId);
+                    this.session.TransactionContext.AddSynchronization(
+                        new ConsumerCloseSynchronization(this));
+                }
+                else
+                {
+                    Tracer.DebugFormat("Consumer {0} No Active TX closing normally.",
+                                       this.ConsumerId);
+                    this.DoClose();                            
+                }
+            }
+        }
+    }
+}
