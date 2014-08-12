a0f256c91638754a27a17df8abc2f8443e136143
Timothy A. Bish <tabish@apache.org>
2014-08-12 17:44:50 +0000

    https://issues.apache.org/jira/browse/AMQNET-488
    apply fix for potential deadlock on failover.

--- src/test/csharp/Threads/CompositeTaskRunnerTest.cs
+++ src/test/csharp/Threads/CompositeTaskRunnerTest.cs
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 
+using System;
 using System.Threading;
 using Apache.NMS.ActiveMQ.Threads;
 using NUnit.Framework;
@@ -24,7 +25,6 @@ namespace Apache.NMS.ActiveMQ.Test.Threads
     [TestFixture]
     public class CompositeTaskRunnerTest
     {
-
         class CountingTask : CompositeTask
         {
             private int count;
@@ -89,6 +89,60 @@ namespace Apache.NMS.ActiveMQ.Test.Threads
 
             runner.RemoveTask(task1);
             runner.RemoveTask(task2);
+        }
+
+        [Test]
+        public void CompositeTaskRunnerDoesntHoldLockWhileCallingIterate()
+        {
+            object lockObj = new object();
+            
+            // Start a task running that takes a shared lock during it's iterate.
+            CompositeTaskRunner runner = new CompositeTaskRunner();
+            runner.AddTask(new LockingTask(lockObj));
+           
+            // Start a separate thread that holds that same lock whilst manipulating the CompositeTaskRunner (See InactivityMonitor for real example).
+            AutoResetEvent resetEvent = new AutoResetEvent(false);
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    lock (lockObj)
+                    {
+                        var countingTask = new CountingTask("task1", 100);
+                        runner.AddTask(countingTask);
+                        runner.RemoveTask(countingTask);
+                    }
+                }
+
+                resetEvent.Set();
+            });
+
+            // Wait for the second thread to finish 10000 attempts.
+            Assert.That(resetEvent.WaitOne(TimeSpan.FromSeconds(10)), "The secondary lock user didn't finish 10000 iterations in less than 10 seconds. Probably dead locked!");
+            runner.Shutdown();
+        }
+
+        private class LockingTask : CompositeTask
+        {
+            private readonly object _lockObj;
+
+            public LockingTask(object lockObj)
+            {
+                _lockObj = lockObj;
+                IsPending = true;
+            }
+
+            public bool Iterate()
+            {
+                lock (_lockObj)
+                {
+                    Thread.Sleep(10);
+                }
+                return true;
+            }
+
+            public bool IsPending { get; private set; }
         }
     }
 }
