8f7019efd8c86e095f19a43ae36df5bbf2c29d7f
Havret <h4vret@gmail.com>
2023-02-04 13:42:38 +0100

    NO-JIRA: Update version to 2.0.1

--- package.ps1
+++ package.ps1
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 $pkgname = "Apache.NMS.ActiveMQ"
-$pkgver = "2.0.0"
+$pkgver = "2.0.1"
 $frameworks = "netstandard2.0"
 
 write-progress "Creating package directory." "Initializing..."
