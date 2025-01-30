8944c4126dfafee61ffcb4c99d21920cf4b5740b
Havret <h4vret@gmail.com>
2025-01-30 11:21:52 +0100

    AMQNET-844 Allow, deny types fix

--- src/NmsDefaultDeserializationPolicy.cs
+++ src/NmsDefaultDeserializationPolicy.cs
@@ -48,10 +48,10 @@ namespace Apache.NMS.ActiveMQ
         
         public bool IsTrustedType(IDestination destination, Type type)
         {
-            var typeName = type?.FullName;
+            var typeName = type.FullName;
             if (typeName == null)
             {
-                return true;
+                return false;
             }
 
             foreach (var denyListEntry in denyList)
